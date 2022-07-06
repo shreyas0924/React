cd01513094305dc6c2e8790394cc9dda3dc8fea5
shreyas0924 <shreyasp0924@gmail.com>
2022-07-06 19:42:46 +0530

    props-multiple

--- Components/src/CommentDatail.js
+++ Components/src/CommentDatail.js
@@ -14,7 +14,7 @@ const CommentDetail = (props) => {
                     {props.author}
                 </a>
                 <div className="metadata">
-                    <span className="date">Today at 6:00pm</span>
+                    <span className="date">{props.timeAgo}</span>
                 </div>
                 <div className="text">Nice blog post</div>
             </div>
--- Components/src/index.js
+++ Components/src/index.js
@@ -8,9 +8,9 @@ const App = () => {
     
     return ( 
         <div className="ui conatainer comments"> 
-            <CommentDetail author = "Sam"/>
-            <CommentDetail author = "Alex"/>
-            <CommentDetail author = "Jane"/>
+            <CommentDetail author = "Sam" timeAgo = "Today at 4:50pm"/>
+            <CommentDetail author = "Alex" timeAgo = "Today at 2:40pm"/>
+            <CommentDetail author = "Jane" timeAgo = "Today at 5:43pm"/>
             
         </div>
     ) ;
