c0de1c508dcafa380030af0d66e03afd1d80510b
shreyas0924 <shreyasp0924@gmail.com>
2022-07-07 19:26:43 +0530

    weather1

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,10 +5,7 @@ import SeasonDisplay from "./SeasonDisplay"
 
 
 class App extends React.Component{
-   
-
-    state = { lat: null, errorMessage:''}
-
+    state = { lat: null, errorMessage:''} ;
    componentDidMount(){
     window.navigator.geolocation.getCurrentPosition(
         position => this.setState({lat : position.coords.latitude}),
@@ -26,23 +23,11 @@ class App extends React.Component{
 
         if(this.state.lat && !this.state.errorMessage){
             return <SeasonDisplay lat={this.state.lat} />;  
-            
             //We are taking property from the state on the app component and passing it as prop into the season display
             // We can take state from one component and pass it as prop down to the child.
             // If we call the setState, the component rerender itself but in addition the coomponent will also rerender any children that it is showing.
-
         }
-
         return <div>Loading</div>
-       
-        // return(
-        //     <div> 
-        //         Latitude: {this.state.lat}
-        //         <br />
-        //         Error : {this.state.errorMessage}
-        //     </div>
-    
-        // );
     }
 };
 
