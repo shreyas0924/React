f49c9846938c9e73538a006c09b8f51e00ee0b9f
shreyas0924 <shreyasp0924@gmail.com>
2022-07-15 19:30:00 +0530

    images count

--- pics1/src/components/App.js
+++ pics1/src/components/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
   state = {images: [] };
 
-  async onSearchSubmit(term) {
-    const response = await axios.get('https://api.unsplash.com/search/photos', {
-      params: { query: term },
-      headers: {
-        Authorization:
-          'Client-ID 2b98c1afb0aed3b3d94a1866bdc3ac013d21a0c86d236a0fee32355c331c0296',
-      },
-    });
-    this.setState({images: response.data.results});
+  onSearchSubmit = async (term) => {
+    const response = await unsplash.get('/search/photos', { params: { query: term } });
+
+    this.setState({ images: response.data.results});
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        
         Found: {this.state.images.length} images
       </div>
     );
