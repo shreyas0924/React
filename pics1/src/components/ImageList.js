f7a1d6d91286956a5d23a48dd71ca14e151a0599
shreyas0924 <shreyasp0924@gmail.com>
2022-07-19 20:50:28 +0530

    image placement

--- pics1/src/components/ImageList.js
+++ pics1/src/components/ImageList.js
@@ -1,14 +1,15 @@
+import './imageList.css';
 import React from "react";
-
+import ImageCard from './imageCard';
 
 
 const ImageList = (props) => {
 
-    const images = props.images.map(({description,id,urls}) => {
-        return <img  alt={description} key={id} src={urls.regular} />
+    const images = props.images.map(image => {
+        return <ImageCard  key={image.id} image={image} />
     });
 
-    return <div>{images}</div>;
+    return <div className='image-list'>{images}</div>;
 }
 
 
