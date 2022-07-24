5be062417df8113cc7c6a35e4c98676157cbc66a
shreyas0924 <shreyasp0924@gmail.com>
2022-07-24 20:30:59 +0530

    acccordion1

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -12,30 +12,30 @@ const Accordion = ({items}) => {
     }
 
     const renderedItems = items.map( (item,index) => {
+
+        const active = index === activeIndex ? 'active' : '' ;
+
         return(
             <React.Fragment key={item.title}>
                 <div 
                     onClick={() => onTitleClick(index)} 
-                    className="title active"
+                    className={`title ${active}`}
                 >
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
 
-                <div>
-                    <div               
-                        className="content active"
-                    >
-                        <p>{item.content}</p>
-                    </div>
+                
+                <div className={`content ${active}`}>
+                    <p>{item.content}</p>
                 </div>
+                
             </React.Fragment>
         ); 
     })
 
     return <div className="ui styled accordion">
         {renderedItems}
-        <h1>{activeIndex}</h1>
     </div>
 }
 
